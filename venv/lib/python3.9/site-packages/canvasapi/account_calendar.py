c7b41b0dbab6a2ebccda284fc522b28a631b7c1b
Abhiram Ghanta <abhiramg2021@gmail.com>
2024-02-11 17:08:33 -0500

    remove venv

--- venv/lib/python3.9/site-packages/canvasapi/account_calendar.py
+++
@@ -1,6 +0,0 @@
-from canvasapi.canvas_object import CanvasObject
-
-
-class AccountCalendar(CanvasObject):
-    def __str__(self):
-        return "{} {} ({})".format(self.name, self.visible, self.calendar_event_url)
--- venv/lib/python3.9/site-packages/canvasapi/jwt.py
+++
@@ -1,6 +0,0 @@
-from canvasapi.canvas_object import CanvasObject
-
-
-class JWT(CanvasObject):
-    def __str__(self):
-        return "{}".format(self.token)
